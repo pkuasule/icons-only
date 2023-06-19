25209b981815af09194bef820756f79633a35017
pkuasule <pkuasule@users.noreply.github.com>
2023-06-19 15:27:16 +0800

    Update check_diff.sh

--- scripts/check_diff.sh
+++ scripts/check_diff.sh
@@ -3,9 +3,11 @@
 # 0. Get full path of this script file √
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
-echo -e "\033[34m【ICONS-ONLY】\033[0mDetected script path as ${SCRIPTPATH}."
 
 cd ${SCRIPTPATH}
 
 # output increments
 diff index_fetch.txt index.txt > increments.txt
+
+cat <<EOF >> index.txt
+EOF
