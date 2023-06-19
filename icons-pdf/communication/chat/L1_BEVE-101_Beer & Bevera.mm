7878d802c4fe1f127c10bf085b6143b8b755b906
pkuasule <pkuasule@users.noreply.github.com>
2023-06-19 15:20:51 +0800

    test

---
+++ icons-pdf/communication/chat/L1_BEVE-101_Beer & Bevera.mm
@@ -0,0 +1,104 @@
+
+<map>
+  <node ID="root" TEXT="L1_BEVE-101_Beer &amp; Beverage Knowledge_啤酒与饮料知识_Mindmap">
+    <node TEXT="一、餐厅中水和服务的基本知识 Water_Basic knowledge and service in a restaurant" ID="ca4771a247024a6b0d65757e0a98731e" STYLE="bubble" POSITION="right">
+      <node TEXT="1、瓶装水的定义" ID="e1f3abc75dbfac3580b7ecdc58426d0a" STYLE="fork"/>
+      <node TEXT="2、餐厅里的瓶装水" ID="8e9271e6d12453489b5a716cbe22f550" STYLE="fork"/>
+      <node TEXT="3、天然矿泉水和纯净水的区别" ID="987228e9bfd2bd110d85e20947e10f7d" STYLE="fork"/>
+      <node TEXT="4、自来水和过滤水的区别" ID="02878b203a1159ce6162859e38cb2ead" STYLE="fork"/>
+      <node TEXT="5、气泡水" ID="74fa8edd0ce9d6f6c2c8c2516486935d" STYLE="fork"/>
+      <node TEXT="6、碳酸水" ID="cb3fc623de4e4b3345f3d20abf9668b9" STYLE="fork"/>
+      <node TEXT="7、餐厅的水服务" ID="2d4bca5270e3ba0341254b6831f0cb89" STYLE="fork"/>
+    </node>
+    <node TEXT="二、餐厅软饮料和服务的基础知识 Soft drinks basic knowledge" ID="29e9962c41db2bb1f4d1b7bdeebcdfc4" STYLE="bubble" POSITION="right">
+      <node TEXT="1、软饮料的定义" ID="d8a3392348b4894ebb34f03c8e4e3dab" STYLE="fork"/>
+      <node TEXT="2、软饮料的含量" ID="a7d47cf11cbf9ba2142e020f2a2454bb" STYLE="fork"/>
+      <node TEXT="3、软饮料的市场" ID="3c782dad97e531416913814d771fb82c" STYLE="fork">
+        <node TEXT="可口可乐" ID="335c63c9d6ca942c32d66985693db540" STYLE="fork"/>
+        <node TEXT="百事可乐" ID="5d2ce3d20fd04506636043f63b681c50" STYLE="fork"/>
+        <node TEXT="红牛公司" ID="b2126c22e0eb6ccecf30829ad78fe0d5" STYLE="fork"/>
+      </node>
+      <node TEXT="4、软饮料的包装选项" ID="835ab985638d4598571211433616c1e9" STYLE="fork"/>
+      <node TEXT="5、预混合和后混合系统的区别" ID="cdb8a18b31bbb0fc91759cea82a110e9" STYLE="fork"/>
+    </node>
+    <node TEXT="三、无酒精饮料：果汁 Non alcoholic beverage_Juices" ID="c61f324245f6d99f70fb4108739363a9" STYLE="bubble" POSITION="right">
+      <node TEXT="1、无酒精饮料的介绍" ID="74013691d53a767297b9d99bbacc99fe" STYLE="fork"/>
+      <node TEXT="2、果汁的饮用益处" ID="d68f5e7f82528d9ebe8ca3bc427ff42e" STYLE="fork"/>
+      <node TEXT="3、不同的果汁种类" ID="28a8a13cfc330c2f7de40a260b481110" STYLE="fork"/>
+      <node TEXT="4、果汁制作过程" ID="f9401b8e232878b2f1cab2ecbc87d6b7" STYLE="fork">
+        <node TEXT="巴氏消菌" ID="e51aa1e268ad41aef5d2a407aa795011" STYLE="fork"/>
+      </node>
+      <node TEXT="5、果汁制作工艺 - 浓缩果汁" ID="875d52358c3198af14ab378fa0ab671d" STYLE="fork"/>
+      <node TEXT="6、果汁制作过程 - 果泥汁" ID="7ab13a031c6f7ee444eb9bebd80353f0" STYLE="fork"/>
+      <node TEXT="7、水果冰沙" ID="7258b257e96edb3ee083dc3ceab3452c" STYLE="fork"/>
+      <node TEXT="8、果汁服务" ID="b8f741a9d103383c261c7bb15d53d6eb" STYLE="fork"/>
+    </node>
+    <node TEXT="四、非酒精饮料：冰茶和咖啡产品 Cold tea and coffee product" ID="a7d44b6e6490aeb09d0edf16809bbcba" STYLE="bubble" POSITION="right">
+      <node TEXT="1、冰茶产品" ID="bccda9e68a21b3e10b4cf4a7fdd8124d" STYLE="fork"/>
+      <node TEXT="2、全球冰茶品牌" ID="f5a13954b174ffe37aae5ba4092a13a6" STYLE="fork"/>
+      <node TEXT="3、其他茶制品" ID="b336bf0d5a19d07a377f7e8e72d48cde" STYLE="fork"/>
+      <node TEXT="4、冰咖啡" ID="cc23d64f700615606aacbd49e0e258aa" STYLE="fork"/>
+      <node TEXT="5、冷萃咖啡" ID="4ffb9eccdac5cd5455dfc3b4d6139946" STYLE="fork"/>
+    </node>
+    <node TEXT="五、啤酒介绍 Beer Introduction" ID="27796e15a124e7b08ec7130eaaa1354d" STYLE="bubble" POSITION="right">
+      <node TEXT="1、啤酒的简介" ID="1da0b42676dd2f630a26c39639d5ebe2" STYLE="fork"/>
+      <node TEXT="2、啤酒的定义" ID="d7699015f230ecc9fa51c43c65b75294" STYLE="fork"/>
+      <node TEXT="3、啤酒的历史" ID="b0ef2ecaadde04136cea7e2a636ec630" STYLE="fork"/>
+      <node TEXT="4、全球啤酒市场简介" ID="9cf76f2ce12c069c157ea9b6d3a9ad58" STYLE="fork"/>
+      <node TEXT="5、全球啤酒品牌简介" ID="422b0116d0b312e43746b92c2aae2433" STYLE="fork"/>
+    </node>
+    <node TEXT="六、啤酒原料和麦芽制造工艺 Beer Ingredient and malting process" ID="ade40f96211fe0463605a87faeaa78ba" STYLE="bubble" POSITION="right">
+      <node TEXT="1、啤酒的主要成分并介绍每部分" ID="655a3c480400942ad2bcfdf1298bd121" STYLE="fork"/>
+      <node TEXT="2、啤酒的制作过程" ID="2b8cfa35176bca1bfbba003b919c2e9e" STYLE="fork"/>
+      <node TEXT="3、从谷物到麦芽" ID="4f96a18ffa96ba4341aec1a0ae1f143b" STYLE="fork"/>
+      <node TEXT="4、不同类型的麦芽" ID="1fbbf57f78d028f4723e5b656e5af5d7" STYLE="fork"/>
+    </node>
+    <node TEXT="七、啤酒生产过程 Beer making process " ID="e82d8e71be0ff07e6a762d6f288be236" STYLE="bubble" POSITION="right">
+      <node TEXT="1、麦芽加工" ID="911adc5ebdb76569e4d3a060069379f9" STYLE="fork"/>
+      <node TEXT="2、糖化" ID="7d8faac46e7985c26aec7a7d1a09a136" STYLE="fork"/>
+      <node TEXT="3、煮沸加酒花" ID="3bb294f7c0dd8c4a07f3a925e46a6e3c" STYLE="fork"/>
+      <node TEXT="4、发酵" ID="c6a725b0ff1696cb944e8df2d409864e" STYLE="fork"/>
+      <node TEXT="5、成熟" ID="53ff27724a52f1862fd0750097bb9882" STYLE="fork"/>
+      <node TEXT="6、过滤" ID="f6dbb12c40fd265bc0c8ec8e074d87ea" STYLE="fork"/>
+      <node TEXT="7、加工" ID="eea491bcd574d2a6d286affcdb66bdff" STYLE="fork"/>
+      <node TEXT="8、拉格啤酒和艾尔啤酒的区别" ID="9c37902bb98c18b868e280c8c123f502" STYLE="fork"/>
+    </node>
+    <node TEXT="八、不同类型的啤酒 Differents type of beers" ID="e44b140f03aa7202441547db261009b2" STYLE="bubble" POSITION="right">
+      <node TEXT="1、拉格啤酒和艾尔啤酒" ID="88d768d43fc18df6aa8914cd41ccda29" STYLE="fork"/>
+      <node TEXT="2、顶部发酵啤酒" ID="54a45380422cd6c577cdfce47cfeb03e" STYLE="fork"/>
+      <node TEXT="3、小麦啤酒（白啤酒）" ID="692d3ad117b37a94916a2cedc086eb89" STYLE="fork"/>
+      <node TEXT="4、司陶特黑啤酒" ID="d45545846087db3b5b2b79d41fc11d8b" STYLE="fork"/>
+      <node TEXT="5、淡色艾尔啤酒" ID="e564298ebc373b1b608104efe2e492aa" STYLE="fork"/>
+      <node TEXT="6、印度淡色艾尔" ID="82ec14dbda47d6bc7300f12fc5fcef86" STYLE="fork"/>
+      <node TEXT="7、底部发酵啤酒" ID="d88e3dbb952b28e665265c6d745a340d" STYLE="fork"/>
+      <node TEXT="8、淡拉格啤酒" ID="c8464a5fb2dd39f5c80948af0185a93b" STYLE="fork"/>
+      <node TEXT="9、比尔森啤酒" ID="bc22f43f571941497937b5304e7e8577" STYLE="fork"/>
+      <node TEXT="10、深色拉格" ID="49b64df8d913073bdcd45aa1663ef2c3" STYLE="fork"/>
+      <node TEXT="11、季节性啤酒" ID="4314c7353514e8ce56647082452b1b40" STYLE="fork"/>
+    </node>
+    <node TEXT="九、啤酒服务 Beer Service" ID="812a8f37665ed7038c84a20b4a586c1b" STYLE="bubble" POSITION="right">
+      <node TEXT="1、啤酒服务的一般知识" ID="cf1d6f900ec79068132f37e70e4be1f6" STYLE="fork"/>
+      <node TEXT="2、从抽头流出的拉格啤酒" ID="60a86eb59d64f0f28b812676f7d409bc" STYLE="fork"/>
+      <node TEXT="3、瓶装拉格啤酒" ID="2fb72173a26230bca78e3baa5ed6dcd3" STYLE="fork"/>
+      <node TEXT="4、黑色拉格啤酒" ID="499afcef12a4bc0a9a45d384355643d5" STYLE="fork"/>
+      <node TEXT="5、艾尔啤酒" ID="86dc79b8a2b4c95710d98e5fb68ca192" STYLE="fork"/>
+      <node TEXT="6、小麦啤酒" ID="4a3d14181f8997dc089d2e0f51f4447d" STYLE="fork"/>
+      <node TEXT="7、啤酒点单" ID="49f91a884b22d28c55a96dd40a7affa1" STYLE="fork"/>
+    </node>
+    <node TEXT="十、啤酒和食物搭配 Beer &amp; Food Matching" ID="7cbf45d640e35e409a4f7c7bb426c5fb" STYLE="bubble" POSITION="right">
+      <node TEXT="1、啤酒侍酒师" ID="14c0ab7e50fca5cd1f971dbbf9bca31a" STYLE="fork"/>
+      <node TEXT="2、啤酒中的主要风味" ID="4bbc6471faecfe7affd84e0b2f1cfb2d" STYLE="fork"/>
+      <node TEXT="3、基本的食物和啤酒搭配规则" ID="e89de6a0a5404813d97dabb506fd5588" STYLE="fork">
+        <node TEXT="根据前面介绍的不同类型啤酒，展示不同类型啤酒应该与什么样的食物搭配" ID="dc7522741a13c1139563676972a4e3d6" STYLE="fork"/>
+      </node>
+    </node>
+    <node TEXT="十一、无酒精鸡尾酒 Non Alcoholic Cocktail" ID="5c7381656f6e842488f38101ab01f5b0" STYLE="bubble" POSITION="right">
+      <node TEXT="1、无酒精饮料的定义" ID="3aefa323ba8e95d95df1a9b8146e178d" STYLE="fork"/>
+      <node TEXT="2、无酒精啤酒" ID="3f3f719f9e4f4b0edfcb60d4df296dd9" STYLE="fork"/>
+      <node TEXT="3、无酒精饮料市场" ID="5f3c49a59e6c510f16395b3371a18e46" STYLE="fork"/>
+      <node TEXT="4、无酒精鸡尾酒" ID="1d2aa705d4744dd7d37025ba609d24b7" STYLE="fork">
+        <node TEXT="介绍3-5种不同的无酒精鸡尾酒饮料" ID="4173cf1c71ef76745dceb31e48946fb8" STYLE="fork"/>
+      </node>
+    </node>
+  </node>
+</map>
